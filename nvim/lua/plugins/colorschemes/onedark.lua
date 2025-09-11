38e6b17ce69049ae87a0edef2963887a3c4bb72c
Andrea Baldini <andrea.baldini@livgemini.com>
2025-09-11 10:04:22 +0200

    Updated font and onedark theme

--- nvim/lua/plugins/colorschemes/onedark.lua
+++ nvim/lua/plugins/colorschemes/onedark.lua
@@ -3,8 +3,12 @@ return {
 	priority = 1000,
 	config = function()
 		require("onedark").setup({
-			style = "darker",
+			style = "warmer",
 		})
 		require("onedark").load()
+	end,
+	init = function()
+		vim.cmd.colorscheme("onedark")
+		vim.cmd.hi("Comment gui=none")
 	end,
 }
